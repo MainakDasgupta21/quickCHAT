56c5022aa207f9cf7aaec263d919e56d1245097e
MAINAK DASGUPTA <123058655+MainakDasgupta21@users.noreply.github.com>
2025-09-16 02:41:19 +0530

    fixed home.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
-import RightSidebar from "../components/RightSIdeBar";
+import RightSidebar from "../components/RightSideBar";
 import { ChatContext } from "../../context/ChatContext";
 
 const HomePage = () => {
