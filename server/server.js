cb5d4069bcf02005a579d60166b631520b8b5602
MAINAK DASGUPTA <123058655+MainakDasgupta21@users.noreply.github.com>
2025-09-16 02:32:18 +0530

    server.js fixed

--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ const server = http.createServer(app)
 
 
 //initialize socket.io server
-export const io = new Server( server ,{
-       cors: {origin: "*"} 
+export const io = new Server(server, {
+        cors: { origin: "*" }
 })
 
 
@@ -29,20 +29,20 @@ export const userSocketMap = {}; //{userId: socketId}
 
 
 //socket.io connection handler
-io.on("connection" , (socket) => {
+io.on("connection", (socket) => {
         const userId = socket.handshake.query.userId;
-        console.log("User Connected" , userId);
+        console.log("User Connected", userId);
 
-        if(userId) userSocketMap[userId] = socket.id;
+        if (userId) userSocketMap[userId] = socket.id;
 
         //Emit online users to all connected client
-        io.emit("getOnlineUsers" , Object.keys(userSocketMap))
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
 
 
-        socket.on("disconnect" , ()=>{
-                console.log("User Disconnected" , userId);
+        socket.on("disconnect", () => {
+                console.log("User Disconnected", userId);
                 delete userSocketMap[userId];
-                io.emit("getOnlineUsers" , Object.keys(userSocketMap))
+                io.emit("getOnlineUsers", Object.keys(userSocketMap))
         })
 })
 
@@ -57,5 +57,11 @@ app.use("/api/messages", messageRouter);
 //connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("server is running on PORT ", PORT));
+
+if (process.env.NODE_ENV !== 'production') {
+        const PORT = process.env.PORT || 5000;
+        server.listen(PORT, () => console.log("server is running on PORT ", PORT));
+}
+
+
+export default server;
